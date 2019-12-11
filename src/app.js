d3239b36e9e0f05923a45ea1e3716061ba5e965e
Nate Kinne <nathan.kinne@gmail.com>
2019-12-10 21:03:05 -0600

    cors fix

--- src/app.js
+++ src/app.js
@@ -23,6 +23,11 @@ app.use(cors());
 // app.use(corsOptions);
 app.options('*', cors());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // use endpoints
 app.use(folderRouter);
